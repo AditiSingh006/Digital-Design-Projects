042b8c7faa05e75a26b539b36f4deac404753528
AditiSingh006 <149304532+AditiSingh006@users.noreply.github.com>
2025-12-07 16:21:45 +0530

    Update Right Shift Register.sv

--- Shift Registers/Right Shift Register.sv
+++ Shift Registers/Right Shift Register.sv
@@ -1,1 +1,15 @@
+module rs_register(q, clk, rst, serial_in);
+  
+  input clk, rst;
+  input serial_in;
+  output reg [4:0] q;
+  
+  always @( posedge clk, posedge rst)
+    
+    if(rst)
+      q <= 5'b00000;
+  
+    else
+      q <= {serial_in, q[4:1]};
 
+endmodule

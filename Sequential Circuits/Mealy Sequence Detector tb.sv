7cfd0d760b6d3896f86899cac9d57be96a49c8f0
AditiSingh006 <149304532+AditiSingh006@users.noreply.github.com>
2025-12-12 19:10:21 +0530

    Update Mealy Sequence Detector tb.sv

--- Sequential Circuits/Mealy Sequence Detector tb.sv
+++ Sequential Circuits/Mealy Sequence Detector tb.sv
@@ -1,1 +1,59 @@
+`timescale 1ns/1ns
 
+module tb_seq_d001;
+
+  reg clk, rst, inp;
+  wire det;
+
+  //dut
+  seq_d001 dut(
+    .clk(clk),
+    .rst(rst),
+    .inp(inp),
+    .det(det)
+  );
+
+  //clock generation
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    $display("\tTime\t clk rst inp det");
+    $monitor("\t%0t\t %b   %b   %b   %b",
+              $time, clk, rst, inp, det);
+
+    //initial state
+    rst = 1; inp = 0;
+    #12;
+    rst = 0;
+
+    //test sequence : 0 0 1 pattern detection (001)
+
+    //sequence 1: 0 0 1 = should detect
+    inp = 0; #10;
+    inp = 0; #10;
+    inp = 1; #10;
+
+    //sequence 2: Random bits
+    inp = 1; #10;
+    inp = 0; #10;
+    inp = 1; #10;
+    inp = 0; #10;
+
+    //sequence 3: Another valid 0 0 1
+    inp = 0; #10;
+    inp = 0; #10;
+    inp = 1; #10;
+
+    //sequence 4: Noise inputs
+    inp = 1; #10;
+    inp = 1; #10;
+    inp = 0; #10;
+    inp = 0; #10;
+
+    $finish;
+  end
+
+endmodule

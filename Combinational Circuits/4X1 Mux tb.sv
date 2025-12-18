85fcc51bb3e92ad4b8436d2df24bc1525ec455aa
AditiSingh006 <149304532+AditiSingh006@users.noreply.github.com>
2025-12-18 08:09:12 +0530

    Update 4X1 Mux tb.sv

--- Combinational Circuits/4X1 Mux tb.sv
+++ Combinational Circuits/4X1 Mux tb.sv
@@ -1,1 +1,39 @@
+`timescale 1ns/1ns
 
+module tb_mux_4x1_df;
+  
+  reg [3:0] I;
+  reg [1:0] S;
+  wire Y;
+  
+  mux_4x1_df dut(
+    .Y(Y),
+    .I(I),
+    .S(S)
+  );
+  
+  initial begin
+    
+    $display("\t Time \t  I \t S \t Y");
+    $monitor("\t %0t \t %b \t %b \t %b", $time, I, S, Y);
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_mux_4x1_df);
+    
+    I = 4'b 1010;
+    S = 2'b 00; #10;
+    S = 2'b 01; #10;
+    S = 2'b 10; #10;
+    S = 2'b 11; #10;
+    
+    I = 4'b 1101;
+    S = 2'b 00; #10;
+    S = 2'b 01; #10;
+    S = 2'b 10; #10;
+    S = 2'b 11; #10;
+    
+    $finish;
+    
+  end
+  
+endmodule

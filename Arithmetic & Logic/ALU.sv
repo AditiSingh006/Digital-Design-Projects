46b4920136ccca8cb56227c1b5f093b7bd0dba72
AditiSingh006 <149304532+AditiSingh006@users.noreply.github.com>
2025-12-25 11:56:37 +0530

    Update ALU.sv

--- Arithmetic & Logic/ALU.sv
+++ Arithmetic & Logic/ALU.sv
@@ -1,1 +1,49 @@
-
+module ALU(A, B, X, Y, OPCODE)
+  
+  input [3:0] X, Y, OPCODE;
+  output reg [3:0] A, B;
+  
+  always @(X, Y, OPCODE)
+    begin
+      
+      case(OPCODE)
+        
+        4'b0000 : X [0] = | A;
+        
+        4'b0001 : X [0] = & A;
+        
+        4'b0010 : X [0] = ^ A;
+        
+        4'b0011 : X = A | B;
+        
+        4'b0100 : X = A & B;
+        
+        4'b0101 : X = A ^ B;
+        
+        4'b0110 : X [0] = A > B;
+        
+        4'b0111 : X [0] = A < B;
+        
+        4'b1000 : X [0] = A != B;
+        
+        4'b1001 : X [0] = A == B;
+        
+        4'b1010 : {Y[0], X} = A+B;
+        
+        4'b1011 : X = A-B;
+        
+        4'b1100 : {Y, X} = A*B;
+        
+        4'b1101 : {Y, X} = A>>B;
+        
+        4'b1110 : {Y, X} = A<<B;
+        
+        4'b1111 : X = ~A;
+        
+        default : $display("error");
+        
+      endcase
+      
+    end
+  
+endmodule
